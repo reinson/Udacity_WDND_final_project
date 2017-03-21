57be4e78619d3f9808f449126507f18214e7ac9c
Tormi Reinson <tormi.reinson@gmail.com>
2017-03-22 00:17:45 +0200

    New info window height and hidden map type controls

--- js/script.js
+++ js/script.js
@@ -23,6 +23,7 @@ function initMap() {
 
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 58.7413549, lng: 24.9980244},
+            mapTypeControl: true,
             minZoom: 6,
             zoom: 7
         });
